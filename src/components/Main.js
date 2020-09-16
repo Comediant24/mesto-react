f91319cefa2744fe94400263ac2d1ea5663c2792
Iurii Bairamukov <juribai@mail.ru>
2020-09-16 12:39:26 +0700

    feat: подключить API и получить userInfo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import api from '../utils/api';
+console.log('api', api);
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+
+  api.getUserInfo().then((data) => {
+    setUserName(data.name);
+    setUserDescription(data.about);
+    setUserAvatar(data.avatar);
+  });
+
   return (
     <main className="content">
       <section className="profile">
@@ -9,17 +21,21 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
           onClick={onEditAvatar}
           className="button button_non-opacity profile__button-avatar"
         >
-          <img className="profile__avatar" src="#" alt="Аватар пользователя" />
+          <img
+            className="profile__avatar"
+            src={userAvatar}
+            alt="Аватар пользователя"
+          />
         </button>
         <div className="profile__info">
-          <h1 className="profile__user-name"></h1>
+          <h1 className="profile__user-name">{userName}</h1>
           <button
             className="button profile__user-edit-button"
             onClick={onEditProfile}
             type="button"
             aria-label="Редактировать информацию пользователя"
           ></button>
-          <p className="profile__user-job"></p>
+          <p className="profile__user-job">{userDescription}</p>
         </div>
         <button
           className="button profile__add-button"
