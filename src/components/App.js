b7ad0818c73bd2146a5d2725a4ca89cdff02446a
Iurii Bairamukov <juribai@mail.ru>
2020-09-19 19:04:45 +0700

    feat: исправить стили

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import PopupWithForm from './PopupWithForm';
-import '../index.css';
 import ImagePopup from './ImagePopup';
 
 function App() {
